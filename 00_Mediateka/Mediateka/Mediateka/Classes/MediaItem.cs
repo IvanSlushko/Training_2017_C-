efea164f421ce7571a4c82c1bfcd689e456ee2ee
IvanSlushko <lu813@tut.by>
2017-04-09 18:33:23 +0300

    no message

---
+++ 00_Mediateka/Mediateka/Mediateka/Classes/MediaItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mediateka.Interfaces;
+
+namespace Mediateka.Builders.Classes
+{
+    abstract class MediaItem: IMediaItem
+    {
+        public MediaItem (string name, string Url)
+        {
+
+        }
+    }
+}
---
+++ 00_Mediateka/Mediateka/Mediateka/Interfaces/IMediaItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Interfaces
+{
+    class IMediaItem
+    {
+        string Name { get; }
+        string Url { get; }
+    }
+}
