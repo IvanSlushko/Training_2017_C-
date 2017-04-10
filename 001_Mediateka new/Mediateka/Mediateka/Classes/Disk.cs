e8f340b903c086adb6d76d3bd19bc321fd6d03c7
IvanSlushko <lu813@tut.by>
2017-04-10 23:34:02 +0300

    add disk

---
+++ 001_Mediateka new/Mediateka/Mediateka/Classes/Disk.cs
@@ -0,0 +1,30 @@
+﻿using Mediateka.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediateka.Classes
+{
+    public class Disk
+    {
+        public List<IDisk> Tracks { get; private set; }
+
+        public Disk()
+        {
+            Tracks = new List<IDisk>();
+        }
+        public Disk(List<IDisk> tracks)
+        {
+           Tracks = tracks;
+        }
+
+        public string AddTrack(IDisk Tracks)
+        {
+            Tracks.Add(Tracks);
+            return "Track added" + Tracks;
+        }
+
+    }
+}
