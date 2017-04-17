80f81ae3a84b91ca8fd11953095835ad4c8dc6b7
IvanSlushko <lu813@tut.by>
2017-04-17 21:14:33 +0300

    no message

---
+++ 01_NY_Present/NewYearPresent/NewYearPresent/GiftElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearPresent
+{
+    public abstract class GiftElement
+    {
+        public string name { get; private set; }
+        public int weith { get; private set; }
+        public int sugar { get; private set; }
+        public int calories { get; private set; }
+
+        public GiftElement(string name, int weith, int sugar, int calories)
+        {
+
+        }
+
+    }
+}
