4b161e26e732df80caef0007dcca382ef4fe7c2e
IvanSlushko <lu813@tut.by>
2017-04-17 22:07:44 +0300

    add WaffleElement

--- 01_NY_Present/NewYearPresent/NewYearPresent/Elements/CandyElement.cs
+++ 01_NY_Present/NewYearPresent/NewYearPresent/Elements/CandyElement.cs
@@ -2,15 +2,14 @@
 
 namespace NewYearPresent
 {
-
     public class CandyElement : GiftElement
     {
-        public enum TypeCandyElement { ChocolateCandy, DropCandy , Sweetmeat };
+        public enum TypeCandyElement { ChocolateCandy, DropCandy, Sweetmeat };
         public TypeCandyElement type;
-        
+
 
         public CandyElement(string elementName, int elementWeith, int elementSugar,
-            int elementCalories, TypeCandyElement type) : 
+            int elementCalories, TypeCandyElement type) :
             base(elementName, elementWeith, elementSugar, elementCalories)
         {
             this.type = type;
---
+++ 01_NY_Present/NewYearPresent/NewYearPresent/Elements/WaffleElement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NewYearPresent.Elements
+{
+    class WaffleElement : GiftElement
+    {
+        public enum TypeWaffleElement { };
+        public TypeWaffleElement type;
+
+        public WaffleElement(string elementName, int elementWeith, int elementSugar,
+            int elementCalories, TypeWaffleElement type) :
+            base(elementName, elementWeith, elementSugar, elementCalories)
+        {
+            this.type = type;
+        }
+
+        public override void TypeGiftElement()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
