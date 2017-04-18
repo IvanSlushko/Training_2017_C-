20654213a7783b150277374a324ad24f962375ec
IvanSlushko <lu813@tut.by>
2017-04-18 22:13:20 +0300

    no message

---
+++ 01_NY_Present/NewYearPresent/NewYearPresent/Creator/Creator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearPresent.Creator
+{
+    abstract class Creator
+    {
+
+
+        public abstract GiftElement BuildCandy();
+
+
+    }
+}
