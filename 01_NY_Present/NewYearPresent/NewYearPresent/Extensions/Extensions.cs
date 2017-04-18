c95b1955b20e93844ec8e8574b0cae5fd62b71b6
IvanSlushko <lu813@tut.by>
2017-04-18 21:59:33 +0300

    add Extensions

---
+++ 01_NY_Present/NewYearPresent/NewYearPresent/Extensions/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewYearPresent.Extensions
+{
+    class Extensions
+    {
+
+    //    namespace ConsoleApplication8
+    //{    ПРИМЕР ВЫВОДА урок про лямбда и линк
+    //    public static class Extensions
+    //    {
+    //        public static IEnumerable<T> ToConsole<T>(this IEnumerable<T> source, Func<T, string> projector)
+    //        {
+    //            foreach (var p in source)
+    //            {
+    //                Console.WriteLine(projector(p));
+    //                yield return p;
+    //            }
+    //        }
+    //    }
+    //}
+
+
+}
+}
--- 01_NY_Present/NewYearPresent/NewYearPresent/Gift/Gift.cs
+++ 01_NY_Present/NewYearPresent/NewYearPresent/Gift/Gift.cs
@@ -21,17 +21,35 @@ namespace NewYearPresent.Gift
             }
         }
 
-        public void Add (GiftElement giftElements)
+        public void Add(GiftElement giftElements)
         {
             elements.Add(giftElements);
         }
 
         //int result1 = Sum(integers, x => x > 5);
         //int result2 = Sum(integers, x => x % 2 == 0);
+        //var q2 = orders.Join(
+        //        users,
+        //        order => order.UserId,
+        //        user => user.Id,
+        //        (order, user) => new { OrderId = order.Id, UserName = user.Name, OrderSum = order.Sum }
+        //            ).ToConsole(x => String.Format("{0} {1}", x.OrderId, x.UserName));
 
+
+
+
+        //        Sum with Selector
+
+        //This example sums lengths of all strings in the list.
+
+        //var stringList = new List<string> { "88888888", "22", "666666", "333" };
+
+        //        // these two lines do the same
+        //        int lengthSum = stringList.Select(x => x.Length).Sum();  // lengthSum: 19
+        //        int lengthSum = stringList.Sum(x => x.Length);           // lengthSum: 19
         public int GiftWeith()
         {
-            return elements.Sum.ToConsole();
+            return elements.Sum(x => x.weith);
         }
     }
 }
