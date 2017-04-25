6a6d686dd2bff326b05b2b40f50572eba31d1798
IvanSlushko <lu813@tut.by>
2017-04-25 21:02:06 +0300

    no message

--- 01_NY_Present/NewYearPresent/NewYearPresent/Extensions/SplitText.cs
+++ 01_NY_Present/NewYearPresent/NewYearPresent/Extensions/SplitText.cs
@@ -11,10 +11,18 @@ namespace NewYearPresent.Extensions
 {
     public static class SplitText
     {
+        //private static string substring;
+
         public static void Go()
         {
 
             string[] lines = System.IO.File.ReadAllLines(@"C:\Lines.txt");
+
+            Creator[] variants = new Creator[3];
+            variants[0] = new CandyElementCreator();
+            variants[1] = new ChokoElementCreator();
+            variants[2] = new WaffleElementCreator();
+            IGift gift = new Gift.Gift();
 
             foreach (string line in lines)
             {
@@ -22,46 +30,40 @@ namespace NewYearPresent.Extensions
                 Console.WriteLine(line);
                 //Console.WriteLine(line.Length);
 
-               
-
-                Creator[] variants = new Creator[3];
-                variants[0] = new CandyElementCreator();
-                variants[1] = new ChokoElementCreator();
-                variants[2] = new WaffleElementCreator();
-
                 Char delimiter = ',';
                 String[] substrings = line.Split(delimiter);
 
-                IGift gift = new Gift.Gift();
 
-                foreach (var substring in substrings)
+                //Console.WriteLine(substrings.Length);
+
+                for (int i = 0; i < substrings.Length; i++)
                 {
-                    // Console.WriteLine(substring);
-
-                    //убираю пробелы
-                    var substring1 = Regex.Replace(substring, @"\s+", "");
-
-                    int res;
-                    bool isInt = int.TryParse(substring1, out res);
-
-                    if (isInt == true) {
-                        Console.WriteLine("Цифра  {0}", res);
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("Текст  {0}", substring1);
-                    }
-
-
-
-
-                    //bool IsDigit = substring.Length == substring.Where(c => char.IsDigit(c)).Count();
-
-
-
-
+                    var substring1 = Regex.Replace(substrings[i], @"\s+", "");
+                    //substring1 = Regex.Replace(substrings[i], @",", "");
+                    Console.WriteLine("-->{0}, ",substring1);
                 }
+                
+
+
+                //foreach (var substring in substrings)
+                //{
+                //    // Console.WriteLine(substring);!!!!!
+                //    //убираю пробелы
+                //    var substring1 = Regex.Replace(substring, @"\s+", "");
+                //    int res;
+                //    bool isInt = int.TryParse(substring1, out res);
+
+                //    if (isInt == true)
+                //    {
+                //        Console.WriteLine("Цифра  {0}", res);
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("Текст  {0}", substring1);
+                //    }
+
+                //    //bool IsDigit = substring.Length == substring.Where(c => char.IsDigit(c)).Count();
+                //}
 
                 Console.WriteLine(new string('-', 20));
 
