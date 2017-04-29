bd71a42b32bedd4636ff4840c11f1e189cf4ab3a
IvanSlushko <lu813@tut.by>
2017-04-29 11:16:55 +0300

    no message

--- 01_NY_Present/NewYearPresent/NewYearPresent/Creators/Creator.cs
+++ 01_NY_Present/NewYearPresent/NewYearPresent/Creators/Creator.cs
@@ -1,5 +1,7 @@
 ﻿using NewYearPresent.Enum;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using static NewYearPresent.CandyElement;
 using static NewYearPresent.ChocoElement;
 using static NewYearPresent.WaffleElement;
@@ -8,6 +10,36 @@ namespace NewYearPresent.Creators
 {
     public abstract class Creator
     {
+
+        IDictionary<string, GiftElement> diction = new Dictionary<string, GiftElement>();
+
+       // diction.
+
+    //    _dictionary.Add(  "ChocolateCandy", TypeCandyElement.ChocolateCandy);
+    //_dictionary.Add(  "DropCandy", TypeCandyElement.DropCandy);
+    //        _dictionary.Add(  "Sweetmeat", TypeCandyElement.Sweetmeat);
+           
+            
+           
+        //IDictionary<string, CreateObjMethod> _dictionary = new Dictionary<string, CreateObjMethod>();
+        //IDictionary<string, Enumer> _d1
+        //{
+        //    { ChocolateCandy, 
+        //            , DropCandy
+        //            , Sweetmeat },
+
+        //}
+
+        //protected Enumer GetEnumByString(string param)
+        //{
+            
+        //}
+
+
+
+
+
+
         public abstract GiftElement Build(
             string elementName,
             int elementWeigth,
