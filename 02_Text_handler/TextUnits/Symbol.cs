a29be0a878aae22fcbb7d13a28a396e3ee739dc5
IvanSlushko <lu813@tut.by>
2017-05-03 21:25:38 +0300

    no message

---
+++ 02_Text_handler/TextUnits/Symbol.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextHandler.TextUnits
+{
+    public struct Symbol
+
+    {
+        public string Chars { get; set; }
+
+        public Symbol(string chars)
+        {
+            Chars = chars;
+        }
+
+
+
+
+
+
+    }
+}
