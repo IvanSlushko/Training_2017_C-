bbec60e67a3fd0437d5be95466ef2b6af302c4a8
IvanSlushko <lu813@tut.by>
2017-05-04 22:09:47 +0300

    add abstract parser  and textParser

---
+++ 02_Text_handler/Parsers/Parser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextHandler.Interfaces;
+using TextHandler.TextUnits;
+
+namespace TextHandler.Parsers
+{
+    public abstract class Parser
+    {
+        public abstract Text Parse(StreamReader fileReader);
+        public abstract ISentence ParseSentence(string sentence);
+
+    }
+}
---
+++ 02_Text_handler/Parsers/TextParser.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextHandler.Interfaces;
+using TextHandler.TextUnits;
+
+namespace TextHandler.Parsers
+{
+    public class TextParser : Parser
+    {
+        //// Define a case-insensitive regular expression for repeated words.
+        //Regex rxInsensitive = new Regex(@"\b(?<word>\w+)\s+(\k<word>)\b",
+        //  RegexOptions.Compiled | RegexOptions.IgnoreCase);
+        //// Define a case-sensitive regular expression for repeated words.
+        //Regex rxSensitive = new Regex(@"\b(?<word>\w+)\s+(\k<word>)\b",
+        //  RegexOptions.Compiled);
+
+
+        public override Text Parse(StreamReader fileReader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISentence ParseSentence(string sentence)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+
+
+
+
+}
