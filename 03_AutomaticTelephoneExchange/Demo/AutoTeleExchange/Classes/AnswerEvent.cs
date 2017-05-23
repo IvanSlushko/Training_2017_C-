6460d5d0bfc4a8842426118db43180a069f8457d
IvanSlushko <lu813@tut.by>
2017-05-23 19:44:50 +0300

    no message

---
+++ 03_AutomaticTelephoneExchange/Demo/AutoTeleExchange/Classes/AnswerEvent.cs
@@ -0,0 +1,31 @@
+﻿using AutoTeleExchange.Enums;
+using AutoTeleExchange.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoTeleExchange.Classes
+{
+    public class AnswerEvent : ICallEvent 
+    {
+        public CallStatus InCall;
+        public int TelephoneNumber { get; private set; }
+        public int TargetTelephoneNumber { get; private set; }
+        public Guid Id { get; private set; }
+        public AnswerEvent(int number, int target, CallStatus status)
+        {
+            TelephoneNumber = number;
+            TargetTelephoneNumber = target;
+            InCall = status;
+        }
+        public AnswerEvent(int number, int target, CallStatus status, Guid id)
+        {
+            TelephoneNumber = number;
+            TargetTelephoneNumber = target;
+            InCall = status;
+            Id = id;
+        }
+    }
+}
