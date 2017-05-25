451bac26239fda6ff34a5bc2f81f435737d7b967
IvanSlushko <lu813@tut.by>
2017-05-25 21:00:22 +0300

    no message

---
+++ 03_AutomaticTelephoneExchange/Demo/ReportCreator/Interfaces/IReportCreator.cs
@@ -0,0 +1,19 @@
+﻿using AutoTeleExchange.Enums;
+using ReportCreat.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportCreat.Interfaces
+{
+    public interface IReportCreator
+    {
+        void Create(Report report);
+        IEnumerable<ReportEvent> SortByCallType(Report report);
+        IEnumerable<ReportEvent> SortByDate(Report report);
+        IEnumerable<ReportEvent> SortByCost(Report report);
+        IEnumerable<ReportEvent> SortByNumber(Report report);
+    }
+}
