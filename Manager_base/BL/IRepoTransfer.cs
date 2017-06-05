72ebc30a2c7e207e8f1fc69405876a26d2227768
IvanSlushko <lu813@tut.by>
2017-06-05 22:18:03 +0300

    add repository

---
+++ Manager_base/BL/IRepoTransfer.cs
@@ -0,0 +1,20 @@
+﻿using BL.DataTransfer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public interface IRepoTransfer
+    {
+        void AddSaleInfo(SaleInfo_BL saleInfo_BL);
+        IEnumerable<SaleInfo_BL> GetSales();
+        IEnumerable<Manager_BL> GetManagers();
+        IEnumerable<Client_BL> GetClients();
+        IEnumerable<Product_BL> GetProducts();
+        void UpdateManager(Manager_BL manager_BL);
+        void UpdateSaleInfo(SaleInfo_BL saleInfo_BL);
+    }
+}
---
+++ Manager_base/BL/RepoTransfer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BL.DataTransfer;
+
+namespace BL
+{
+    public class RepoTransfer : IRepoTransfer
+    {
+        public RepoTransfer()
+        {
+           // repositories = new EFUnitOfWork();
+        }
+        public void AddSaleInfo(SaleInfo_BL saleInfo_BL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Client_BL> GetClients()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Manager_BL> GetManagers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product_BL> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<SaleInfo_BL> GetSales()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateManager(Manager_BL manager_BL)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateSaleInfo(SaleInfo_BL saleInfo_BL)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
