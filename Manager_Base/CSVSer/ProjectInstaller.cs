48dbf7bdbe481795e7188a234ba1e157517414a0
IvanSlushko <lu813@tut.by>
2017-06-11 18:16:32 +0300

    no message

--- Manager_Base/CSVSer/ProjectInstaller.cs
+++ Manager_Base/CSVSer/ProjectInstaller.cs
@@ -15,5 +15,10 @@ namespace CSVSer
         {
             InitializeComponent();
         }
+
+        private void serviceProcessInstaller1_AfterInstall(object sender, InstallEventArgs e)
+        {
+
+        }
     }
 }
