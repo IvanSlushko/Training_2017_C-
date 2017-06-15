fb9476ee15931874446e7d797749b590707aa7ce
IvanSlushko <lu813@tut.by>
2017-06-15 23:57:31 +0300

    no message

--- 05_SaleStatistic/SaleStatistics/SaleStatistics/Controllers/HomeController.cs
+++ 05_SaleStatistic/SaleStatistics/SaleStatistics/Controllers/HomeController.cs
@@ -3,34 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BL;
 
 namespace SaleStatistics.Controllers
 {
     public class HomeController : Controller
     {
-
-
         // GET: home
         public ActionResult Index()
         {
-
-            
-
-
+            var repositoryTransfer = new RepoTransfer();
+            ViewBag.Sales = repositoryTransfer.GetSales();
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
+            ViewBag.Message = "Ivan Slushko";
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
+            ViewBag.Message = "lu813@tut.by";
             return View();
         }
     }
