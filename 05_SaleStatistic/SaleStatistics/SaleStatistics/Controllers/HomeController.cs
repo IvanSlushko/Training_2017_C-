e3a11aee1d7b03da334fd96c4c9f414bc15c4052
IvanSlushko <lu813@tut.by>
2017-06-29 00:26:28 +0300

    chart probe

--- 05_SaleStatistic/SaleStatistics/SaleStatistics/Controllers/HomeController.cs
+++ 05_SaleStatistic/SaleStatistics/SaleStatistics/Controllers/HomeController.cs
@@ -261,23 +261,77 @@ namespace SaleStatistics.Controllers
             return PartialView("PartialSalesList", sales);
         }
 
-        [HttpPost]
+        //[HttpGet]
+        //public JsonResult GetManagersChartData()
+        //{
+        //    var repositoryTransfer = new RepoTransfer();
+        //    var sales = repositoryTransfer.GetSales()
+        //                                  .GroupBy(s => s.Manager)
+        //                                  .Select(m => new object[] { m.Key, m.Sum(x => x.PriceSum) })
+        //                                  .ToArray();
+        //    return Json(sales, JsonRequestBehavior.AllowGet); //The value of the transfer: AllowGet - Enable get request
+
+        //}
+
+
+        [HttpGet]
         public JsonResult GetManagersChartData()
         {
-            var repositoryTransfer = new RepoTransfer();
-            var sales = repositoryTransfer.GetSales()
-                                          .GroupBy(s => s.Manager)
-                                          .Select(m => new object[] { m.Key, m.Sum(x => x.PriceSum) })
-                                          .ToArray();
+            List<ChartModel> sales = new List<ChartModel>
+           {
+              new ChartModel { ManagerName = "TomTailor", PriceSum = 28 },
+              new ChartModel { ManagerName = "Tom", PriceSum = 8 },
+              new ChartModel { ManagerName = "Tailor", PriceSum = 2 }
+           };
             return Json(sales, JsonRequestBehavior.AllowGet); //The value of the transfer: AllowGet - Enable get request
 
         }
 
+        private List<ChartModel> GetUsers()
+        {
+            var usersList = new List<ChartModel>
+            {
+                new ChartModel
+                {
+                    ManagerName = "Ram",
+                    PriceSum = 2
+                },
+                new ChartModel
+                {
+                    ManagerName = "chand",
+                    PriceSum = 33
+                },
+                new ChartModel
+                {
+                    ManagerName = "Abc",
+                    PriceSum = 4
+                }
+            };
+
+            return usersList;
+        }
+
+
+        [AllowAnonymous]
+        [HttpGet]
+        public JsonResult GetUsersData()
+        {
+            var users = GetUsers();
+            return Json(users, JsonRequestBehavior.AllowGet);
+        }
+
+        //test
+        public ActionResult ViewChart()
+        {
+            GetUsersData();
+            return View();
+        }
+
+
+
         public ActionResult ManagersChart()
         {
-           // GetManagersChartData();
             return View();
-            
         }
     }
 }
